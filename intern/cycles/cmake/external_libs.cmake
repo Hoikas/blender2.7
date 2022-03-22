91b60066d20b5cb9c049edb3f760b10d30db05ea
Adam Johnson <AdamJohnso@gmail.com>
2022-03-21 21:10:27 -0400

    Update main build to VS 2022.

--- intern/cycles/cmake/external_libs.cmake
+++ intern/cycles/cmake/external_libs.cmake
@@ -38,9 +38,13 @@ endif()
 # CUDA
 
 if(WITH_CYCLES_CUDA_BINARIES OR NOT WITH_CUDA_DYNLOAD)
+	if(MSVC AND MSVC_TOOLSET_VERSION GREATER 142)
+		set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -allow-unsupported-compiler")
+	endif()
 	find_package(CUDA) # Try to auto locate CUDA toolkit
 	if(CUDA_FOUND)
 		message(STATUS "CUDA nvcc = ${CUDA_NVCC_EXECUTABLE}")
+		message(STATUS "CUDA nvcc FLAGS = ${CUDA_NVCC_FLAGS}")
 	else()
 		message(STATUS "CUDA compiler not found, disabling WITH_CYCLES_CUDA_BINARIES")
 		set(WITH_CYCLES_CUDA_BINARIES OFF)
